ce4f1aac79839652f77c8baea5e48299bf6d0e21
Jader Dias <52470+JaderDias@users.noreply.github.com>
2021-08-12 20:37:35 +0200

    add ChessEvent

--- init.go
+++ init.go
@@ -54,3 +54,18 @@ type ChessGame struct {
 type GameBody struct {
 	Game ChessGame `json:"game"`
 }
+
+type ChessEvent struct {
+	Id          int    `json:"id"`
+	Slug        string `json:"slug"`
+	Name        string `json:"name"`
+	ImageUrl    string `json:"imageUrl"`
+	StartAt     string `json:"startAt"`
+	EndAt       string `json:"endAt"`
+	RoundCount  int    `json:"roundCount"`
+	ProcessStep int
+}
+
+type ChessEvents struct {
+	Events []ChessEvent `json:"events"`
+}
