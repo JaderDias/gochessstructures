2d447e20eba7eab59b6f5be75de49d39dc7acdcc
Jader Dias <52470+JaderDias@users.noreply.github.com>
2021-08-12 20:13:12 +0200

    add first structures

---
+++ init.go
@@ -0,0 +1,56 @@
+package chessgostructures
+
+type Country struct {
+	Name string `json:"name"`
+	Code string `json:"code"`
+}
+
+type ChessPlayer struct {
+	Name               string  `json:"name"`
+	Title              string  `json:"title"`
+	FirstName          string  `json:"firstName"`
+	LastName           string  `json:"lastName"`
+	Elo                int     `json:"elo"`
+	AvatarUrl          string  `json:"avatarUrl"`
+	AvatarUrlLarge     string  `json:"avatarUrlLarge"`
+	MasterPlayerUrl    string  `json:"masterPlayerUrl"`
+	Country            Country `json:"country"`
+	FideId             int     `json:"fideId"`
+	Id                 int     `json:"id"`
+	ShortTeam          string  `json:"shortTeam"`
+	Team               string  `json:"team"`
+	FideStandardRating int     `json:"fideStandardRating"`
+	FideRapidRating    int     `json:"fideRapidRating"`
+	FideBlitzRating    int     `json:"fideBlitzRating"`
+	Username           string  `json:"username"`
+}
+
+type ChessMove struct {
+	Id       int    `json:"id"`
+	Ply      int    `json:"ply"`
+	Cbn      string `json:"cbn"`
+	Clock    int    `json:"clock"`
+	Time     int    `json:"time"`
+	Revision int    `json:"revision"`
+}
+
+type ChessGame struct {
+	Id          int         `json:"id"`
+	Slug        string      `json:"slug"`
+	RoundId     int         `json:"roundId"`
+	Result      string      `json:"result"`
+	Table       int         `json:"table"`
+	Board       int         `json:"board"`
+	CurrentFEN  string      `json:"currentFEN"`
+	White       ChessPlayer `json:"white"`
+	Black       ChessPlayer `json:"black"`
+	TimeControl string      `json:"timeControl"`
+	ProcessStep int
+	RoundSlug   string
+	EventSlug   string
+	Moves       []ChessMove `json:"moves"`
+}
+
+type GameBody struct {
+	Game ChessGame `json:"game"`
+}
